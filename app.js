95d3b00df20e7dd3d6186bc3c94a3c734249eaef
Makktu <johnmcna@gmail.com>
2022-05-21 00:50:20 +0100

    node!

--- app.js
+++ app.js
@@ -2,6 +2,18 @@
 
 // *************************************
 
+// const sqlite3 = require("sqlite3").verbose();
+
+// const db = new sqlite3.Database(
+//     "./chinook.db",
+//     sqlite3.OPEN_READWRITE,
+//     (err) => {
+//         if (err) {
+//             return console.error(err.message + "<<<<<<<_________");
+//         } else console.log("ALL OK");
+//     }
+// );
+
 // DO ALL THE DATABASE STUFF HERE
 
 // import sqlite3 from "/sqlite3";
