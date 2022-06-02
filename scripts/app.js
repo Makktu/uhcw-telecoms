e9a4e9a8c78988041e0baeeb7c3a38705c36e906
Makktu <johnmcna@gmail.com>
2022-06-02 13:13:32 +0100

    got rooms working

--- scripts/app.js
+++ scripts/app.js
@@ -2,37 +2,79 @@
 
 // *************************************
 
-const theDatabase = fetch(
-    "https://docs.google.com/spreadsheets/d/1pIuk8dvtgyutXFIpqofqwREYlnptFEIxMuJHZak6FIk/gviz/tq?tqx=out:json&tq&gid=0"
-);
-
-function getResults(searchtext) {
-    var spreadsheetId = "1w3AB-skEn07pVyEj_Auqxhqs3dsvemwMkyNfx8i1vQw"; //** CHANGE !!!
-    var dataRange = "A2:F"; //** CHANGE !!!
-    var data = Sheets.Spreadsheets.Values.get(spreadsheetId, dataRange).values;
-    var ar = [];
-
-    data.forEach(function (f) {
-        f.forEach(function (k) {
-            if (k.includes(searchtext)) {
-                ar.push(f);
-                return;
-            }
-        });
-    });
-    console.log(ar);
-    // return ar;
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    };
+    rawFile.send(null);
 }
+
+readTextFile("./switchdesk1.json", function (text) {
+    roomNums = JSON.parse(text);
+    allData = roomNums.reduce(
+        (acc, entry) => acc.concat(Object.values(entry)),
+        []
+    );
+    console.log("Check: " + allData[0]["Wing"]);
+    dataLoaded = true;
+});
 
 // *************************************
 
-function showOutcome(content) {
-    alertControl.message = content;
-    alertControl.header = "You searched for";
-    alertControl.buttons = ["OK"];
+function showOutcome(searchPhrase) {
+    resArr = [];
+    // msgArea.innerHTML = "";
+    searchPhrase = searchPhrase.toUpperCase();
+    let thisDescription = "";
+    let thisRoom = "";
+    let thisDepartment = "";
+    allData.forEach((entry) => {
+        thisRoom = entry["Room Num"];
+        thisDescription = entry["Description"];
+        thisDepartment = entry["Department"];
+        if (thisRoom?.includes(searchPhrase)) {
+            resArr.push(entry);
+        }
+        if (thisDescription?.includes(searchPhrase)) {
+            resArr.push(entry);
+        }
+        if (thisDepartment?.includes(searchPhrase)) {
+            resArr.push(entry);
+        }
+    });
+
+    if (resArr.length == 0) {
+        html = `Nothing found for '${searchPhrase}'`;
+    } else {
+        if (resArr.length > 1) {
+            html = `Showing 1st result of ${resArr.length}:<br>${resArr[0]["Room Num"]}<br>${resArr[0]["Department"]}<br>${resArr[0]["Wing"]}<br>[Tap ALL To Show All (not working yet)]`;
+            console.log(html);
+        } else {
+            html = `Showing 1 result:<br>${resArr[0]["Room Num"]}<br>${resArr[0]["Department"]}<br>${resArr[0]["Wing"]}`;
+        }
+    }
+    displayBox(html);
+}
+
+function displayBox(html) {
+    alertControl.message = html;
+    alertControl.header = `You searched for: ${searchPhrase.toUpperCase()}`;
+    alertControl.buttons = ["ALL", "OK"];
     document.body.appendChild(alertControl);
     alertControl.present();
 }
+
+let searchPhrase;
+let roomNums = [];
+let resArr = [];
+let dataLoaded = false;
+let allData;
+let html = "<strong>Content Pending</strong>";
 
 const phoneInput = document.querySelector("#input-phone");
 const phoneCancelBtn = document.querySelector("#btn-phone-cancel");
@@ -66,9 +108,9 @@ phoneConfirmBtn.addEventListener("click", () => {
 
 roomConfirmBtn.addEventListener("click", () => {
     if (roomInput.value) {
-        const enteredRoom = roomInput.value;
+        searchPhrase = roomInput.value;
         clearFields();
-        showOutcome(enteredRoom);
+        showOutcome(searchPhrase);
         // getResults(enteredRoom);
     } else {
         alertControl.message = "Enter a search term first";
@@ -90,3 +132,21 @@ roomCancelBtn.addEventListener("click", () => {
         clearFields();
     }
 });
+
+// ************************
+
+// if (thisDepartment?.includes(searchPhrase)) {
+//     resArr.push(entry);
+// }
+
+// if (typeof thisDepartment == "object") {
+//     entry["Department"].forEach((place) => {
+//         if (place.includes(searchPhrase)) {
+//             resArr.push(entry);
+//         }
+//     });
+// } else {
+//     if (entry["Department"].includes(searchPhrase)) {
+//         resArr.push(entry);
+//     }
+// }
