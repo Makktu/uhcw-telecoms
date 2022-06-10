ae539efbbf76794622bf0081f15989a7943f0a46
Makktu <johnmcna@gmail.com>
2022-06-10 02:28:25 +0100

    tweaks to the look

--- scripts/app.js
+++ scripts/app.js
@@ -385,5 +385,94 @@ roomCancelBtn.addEventListener("click", () => {
     }
 });
 
+function presentAlertPrompt() {
+    const alert = document.createElement("ion-alert");
+    alert.cssClass = "my-custom-class";
+    alert.header = "Prompt!";
+    alert.inputs = [
+        {
+            placeholder: "Placeholder 1",
+        },
+        {
+            name: "name2",
+            id: "name2-id",
+            value: "hello",
+            placeholder: "Placeholder 2",
+        },
+        // multiline input.
+        {
+            name: "paragraph",
+            id: "paragraph",
+            type: "textarea",
+            placeholder: "Placeholder 3",
+        },
+        {
+            name: "name3",
+            value: "http://ionicframework.com",
+            type: "url",
+            placeholder: "Favorite site ever",
+        },
+        // input date with min & max
+        {
+            name: "name4",
+            type: "date",
+            min: "2017-03-01",
+            max: "2018-01-12",
+        },
+        // input date without min nor max
+        {
+            name: "name5",
+            type: "date",
+        },
+        {
+            name: "name6",
+            type: "number",
+            min: -5,
+            max: 10,
+        },
+        {
+            name: "name7",
+            type: "number",
+        },
+        {
+            name: "name8",
+            type: "password",
+            placeholder: "Advanced Attributes",
+            cssClass: "specialClass",
+            attributes: {
+                maxlength: 4,
+                inputmode: "decimal",
+            },
+        },
+    ];
+    alert.buttons = [
+        {
+            text: "Cancel",
+            role: "cancel",
+            cssClass: "secondary",
+            handler: () => {
+                console.log("Confirm Cancel");
+            },
+        },
+        {
+            text: "Ok",
+            handler: () => {
+                console.log("Confirm Ok");
+            },
+        },
+    ];
+
+    document.body.appendChild(alert);
+    return alert.present();
+}
+
+window.onload = function () {
+    setTimeout(function () {
+        window.scrollTo(0, 1);
+    }, 0);
+};
+
 let firstUp = false;
 if (!firstUp) firstRun();
+
+// presentAlertPrompt();
